7879ec29769ac2d0e6c332024745196249d874e5
Juany1203 <77596287+Juany1203@users.noreply.github.com>
2021-11-23 17:00:05 -0600

    Added nav bar v1

--- coin/src/components/Navbar/index.js
+++ coin/src/components/Navbar/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Nav,NAvbarContainer, NavbarLogo} from './NavbarElements';
+import {FaBars} from 'react-icons/fa'
+//mport { NavLink } from 'react-router-dom';
+import {Nav,NAvbarContainer, NavbarLogo, MobileIcon, NavMenu, NavLinks, NavItem, NavBtn, NavBtnLink } from './NavbarElements';
 
 const Navbar = () => {
     return (
@@ -7,6 +9,26 @@ const Navbar = () => {
             <Nav>
                 <NAvbarContainer>
                     <NavbarLogo to='/'>C01N</NavbarLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="About">About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="Crypto">Crypto</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="Services">Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="SignUp">Sign Up</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+                    </NavBtn>
                 </NAvbarContainer>
             </Nav>
 
